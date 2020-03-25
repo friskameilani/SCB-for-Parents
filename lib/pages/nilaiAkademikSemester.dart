d1ea1454dcf7e7b1bbcc7ac10554acc05c8d6966
Dhana <dhana_dhira@apps.ipb.ac.id>
2020-03-26 00:21:17 +0700

    Add nilai akademik page, not finished

---
+++ lib/pages/nilaiAkademikSemester.dart
@@ -0,0 +1,17 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class NilaiAkademikSmt extends StatefulWidget{
+  NilaiAkademikSmt(this.semester);
+  final String semester;
+  @override
+  NilaiAkademikSmtState createState() => new NilaiAkademikSmtState();
+}
+
+class NilaiAkademikSmtState extends State<NilaiAkademikSmt>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    
+  }
+}
