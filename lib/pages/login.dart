da1362a6115d51e722e072127197ab1d546f0874
RIZKY SYAWAL PUTRA THAHIR IPB <rizky_syawal@apps.ipb.ac.id>
2020-04-28 20:22:55 +0700

    shortened code

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,84 +6,95 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: ListView(children: <Widget>[
-        SizedBox(
-          height: 80,
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(32.0, 20.0, 32.0, 0.0),
-          child: SafeArea(
-            child: Image.asset("images/Logo-SCB_Hitam.png", width:80, height: 80,),
-          )
-        ),
-        Padding(
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+          ),
+          loginLogo(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(32.0, 20.0, 32.0, 0.0),
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  textBox('Username', 'Masukkan Usernam Anda'),
+                  textBox('Password', 'Masukkan Password Anda'),
+                  loginBtn(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container loginLogo() {
+    return Container(
         padding: const EdgeInsets.fromLTRB(32.0, 20.0, 32.0, 0.0),
         child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Username',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey[800]
-                  ),
-                ),
-                SizedBox(height: 10,),
-                TextFormField( 
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                      borderSide: const BorderSide(color: Colors.green)),
-                    hintText: 'Masukkan Username Anda',
-                  ),
-                ),   
-                SizedBox(height: 16.0,),
-                Text(
-                  'Password',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey[800]
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                TextFormField( 
-                obscureText: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0))),
-                    hintText: 'Masukkan Password Anda',
-                  ),
-                ),
-                SizedBox(height: 16.0,),
-                Container(
-                  child: RaisedButton(
-                    onPressed: (){
-                        Navigator.pushNamed(context, '/beranda');
-                    },
-                    shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(8.0),
-                    ),
-                    child: Text('Masuk',
-                      style: TextStyle(
-                        fontSize: 22,
-                      ),
-                    ),
-                    textColor: Colors.white,
-                    color: Colors.green[800],
-                  ),
-                  height: 60,
-                ),
-              ],
+          child: Image.asset(
+            "images/Logo-SCB_Hitam.png",
+            width: 80,
+            height: 80,
+          ),
+        ));
+  }
+
+  Container textBox(String input, String hint) {
+    return Container(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          Text(
+            input,
+            style: TextStyle(fontSize: 16, color: Colors.grey[800]),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  borderSide: BorderSide(color: Colors.green)),
+              hintText: hint,
             ),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+        ]));
+  }
+
+  Container loginBtn() {
+    return Container(
+      child: RaisedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/beranda');
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(8.0),
         ),
+        child: Text(
+          'Masuk',
+          style: TextStyle(
+            fontSize: 22,
+          ),
+        ),
+        textColor: Colors.white,
+        color: Colors.green[800],
       ),
-      ],),
+      height: 60,
     );
   }
 }
