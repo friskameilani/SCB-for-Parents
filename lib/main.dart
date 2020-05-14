0c9082bdc6cf50ad0a538325ed53f7617e49de85
rizkysyawal9 <rizky_syawal@apps.ipb.ac.id>
2020-05-15 00:37:25 +0700

    added dark mode for fun

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:scbforparents/views/beranda.dart';
 import 'package:scbforparents/views/catatanKhusus.dart';
 import 'package:scbforparents/splash.dart';
@@ -7,26 +8,70 @@ import 'package:scbforparents/views/profil.dart';
 import 'package:scbforparents/views/tabRoutes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'class/darkmode.dart';
+import 'class/themedata.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  DarkThemeProvider themeChangeProvider = new DarkThemeProvider();
+
+  @override
+  void initState() {
+    // super.initState();
+    getCurrentAppTheme();
+  }
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.darkTheme =
+        await themeChangeProvider.darkThemePreference.getTheme();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreen(),
-      // home: CheckAuth(),
-      routes: <String, WidgetBuilder>{
-        //Untuk sementara ubah dulu '/login': (context) => Login(), ke '/login': (context) => Dashboard(),
-        //Kecuali kalian mau coba fitur login pake server lokal, rest API-nya bisa di donlot di github gw
-        '/login': (context) => Login(),
-        '/home': (context) => Home(),
-        '/beranda': (BuildContext context) => Beranda(),
-        '/profil': (context) => Profil(),
-        '/catatanKhusus': (context) => CatatanKhusus(),
+    return ChangeNotifierProvider(
+      create: (_) {
+        return themeChangeProvider;
       },
+      child: Consumer<DarkThemeProvider>(
+          builder: (BuildContext context, value, Widget child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: Styles.themeData(themeChangeProvider.darkTheme, context),
+          home: SplashScreen(),
+          // home: Home(),
+          routes: <String, WidgetBuilder>{
+            //Untuk sementara ubah dulu '/login': (context) => Login(), ke '/login': (context) => Dashboard(),
+            //Kecuali kalian mau coba fitur login pake server lokal, rest API-nya bisa di donlot di github gw
+            // '/login': (context) => Login(),
+            '/login': (context) => Home(),
+            '/beranda': (BuildContext context) => Beranda(),
+            '/profil': (context) => Profil(),
+            '/catatanKhusus': (context) => CatatanKhusus(),
+          },
+        );
+      }),
     );
   }
+
+  // return MaterialApp(
+  //   debugShowCheckedModeBanner: false,
+  //   theme: Styles.themeData(themeChangeProvider.darkTheme, context),
+  //   // home: SplashScreen(),
+  //   home: Home(),
+  //   routes: <String, WidgetBuilder>{
+  //     //Untuk sementara ubah dulu '/login': (context) => Login(), ke '/login': (context) => Dashboard(),
+  //     //Kecuali kalian mau coba fitur login pake server lokal, rest API-nya bisa di donlot di github gw
+  //     // '/login': (context) => Login(),
+  //     '/home': (context) => Home(),
+  //     '/beranda': (BuildContext context) => Beranda(),
+  //     '/profil': (context) => Profil(),
+  //     '/catatanKhusus': (context) => CatatanKhusus(),
+  //   },
+  // );
+  // }
 }
+
 // =========================================================================================================== //
 //Code to be used Later on (Jangan Dihapus yak!)
 //Code ini gabisa diapa2in kecuali di invoke kok
