3a60a6d4d4e4dc262720b222326b963d13c1b420
Dhana <dhana_dhira@apps.ipb.ac.id>
2020-05-17 19:20:29 +0700

    Tried changing transkripasrama.json

--- lib/controllers/nilaiAsrama.dart
+++ lib/controllers/nilaiAsrama.dart
@@ -1,48 +1,85 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
 class NilaiAsrama {
-  String nis, nama, aspek, tahsin, tahfiz, hafalan, hadistTulis,
-      hadistLisan, mufrodat, doaDzikirTulis, doaDzikirLisan, asmaulHusna, talim, hafalanSuratPilihan;
+  String nis;
+  String nama;
+  List<Nilai> nilai;
 
-  NilaiAsrama(this.nis, this.nama, this.aspek,
-      this.tahsin, this.tahfiz, this.hafalan, this.hadistTulis, this.hadistLisan, this.mufrodat,
-      this.doaDzikirTulis, this.doaDzikirLisan, this.asmaulHusna, this.talim, this.hafalanSuratPilihan);
+  NilaiAsrama({this.nis, this.nama, this.nilai});
 
   NilaiAsrama.fromJson(Map<String, dynamic> json) {
     nis = json['nis'];
     nama = json['nama'];
-    aspek = json['aspek'];
-    tahsin = json['aspek']['tahsin'];
-    tahfiz = json['aspek']['tahfiz'];
-    hafalan = json['aspek']['hafalan'];
-    hadistTulis = json['aspek']['hadistTulis'];
-    hadistLisan = json['aspek']['hadistLisan'];
-    mufrodat = json['aspek']['mufrodat'];
-    doaDzikirTulis = json['aspek']['doaDzikirTulis'];
-    doaDzikirLisan = json['aspek']['doaDzikirLisan'];
-    asmaulHusna = json['aspek']['asmaulHusna'];
-    talim = json['aspek']['talim'];
-    hafalanSuratPilihan = json['aspek']['hafalanSuratPilihan'];
+    if (json['nilai'] != null) {
+      nilai = new List<Nilai>();
+      json['nilai'].forEach((v) {
+        nilai.add(new Nilai.fromJson(v));
+      });
+    }
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['nis'] = this.nis;
     data['nama'] = this.nama;
-    data['aspek'] = this.aspek;
-    data['aspek']['tahsin'] = this.tahsin;
-    data['aspek']['tahfiz'] = this.tahfiz;
-    data['aspek']['hafalan'] = this.hafalan;
-    data['aspek']['hadistTulis'] = this.hadistTulis;
-    data['aspek']['hadistLisan'] = this.hadistLisan;
-    data['aspek']['mufrodat'] = this.mufrodat;
-    data['aspek']['doaDzikirTulis'] = this.doaDzikirTulis;
-    data['aspek']['doaDzikirLisan'] = this.doaDzikirLisan;
-    data['aspek']['asmaulHusna'] = this.asmaulHusna;
-    data['aspek']['talim'] = this.talim;
-    data['aspek']['hafalanSuratPilihan'] = this.hafalanSuratPilihan;
+    if (this.nilai != null) {
+      data['nilai'] = this.nilai.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
 
+class Nilai {
+  int tahsin;
+  int tahfiz;
+  int hafalan;
+  int hadistTulis;
+  int hadistLisan;
+  int mufrodat;
+  int doaDanDzikirTulis;
+  int doaDanDzikirLisan;
+  int asmaulHusna;
+  int talim;
+  int hafalanSuratPilihan;
+
+  Nilai(
+      {this.tahsin,
+      this.tahfiz,
+      this.hafalan,
+      this.hadistTulis,
+      this.hadistLisan,
+      this.mufrodat,
+      this.doaDanDzikirTulis,
+      this.doaDanDzikirLisan,
+      this.asmaulHusna,
+      this.talim,
+      this.hafalanSuratPilihan});
+
+  Nilai.fromJson(Map<String, dynamic> json) {
+    tahsin = json['Tahsin'];
+    tahfiz = json['Tahfiz'];
+    hafalan = json['Hafalan'];
+    hadistTulis = json['Hadist Tulis'];
+    hadistLisan = json['Hadist Lisan'];
+    mufrodat = json['Mufrodat'];
+    doaDanDzikirTulis = json['Doa dan Dzikir Tulis'];
+    doaDanDzikirLisan = json['Doa dan Dzikir Lisan'];
+    asmaulHusna = json['Asmaul Husna'];
+    talim = json['Talim'];
+    hafalanSuratPilihan = json['Hafalan Surat Pilihan'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['Tahsin'] = this.tahsin;
+    data['Tahfiz'] = this.tahfiz;
+    data['Hafalan'] = this.hafalan;
+    data['Hadist Tulis'] = this.hadistTulis;
+    data['Hadist Lisan'] = this.hadistLisan;
+    data['Mufrodat'] = this.mufrodat;
+    data['Doa dan Dzikir Tulis'] = this.doaDanDzikirTulis;
+    data['Doa dan Dzikir Lisan'] = this.doaDanDzikirLisan;
+    data['Asmaul Husna'] = this.asmaulHusna;
+    data['Talim'] = this.talim;
+    data['Hafalan Surat Pilihan'] = this.hafalanSuratPilihan;
     return data;
   }
 }
