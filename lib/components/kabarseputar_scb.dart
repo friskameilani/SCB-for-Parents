7b0cc93298af547a9581ad622ec5cef43c1620ba
rizkysyawal9 <rizky_syawal@apps.ipb.ac.id>
2020-05-18 23:41:07 +0700

    erro nampilin data contentful

--- lib/components/kabarseputar_scb.dart
+++ lib/components/kabarseputar_scb.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:scbforparents/models/humasItem.dart';
 import 'dart:async' show Future;
 import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:http/http.dart' as http;
 
 //class KabarSeputarSCB extends StatelessWidget {
 //  final String query = r"""
@@ -63,7 +65,6 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 //  }
 //}
 
-
 class KabarSeputarSCB extends StatefulWidget {
   @override
   _KabarSeputarSCBState createState() => _KabarSeputarSCBState();
@@ -71,18 +72,44 @@ class KabarSeputarSCB extends StatefulWidget {
 
 class _KabarSeputarSCBState extends State<KabarSeputarSCB> {
   List data = [];
+  final String url =
+      'https://cdn.contentful.com/spaces/n2o2oyh78lcv/environments/master/';
+  final String accessToken = 'yNw9k6la9B3Q06h04menoajN6zNllx-ifEK1E8Ia5GU';
 
-  Future<String> loadJsonData() async{
-    var jsonText= await rootBundle.loadString('assets/kabarSeputarSCB.json');
+  Future<String> loadJsonData() async {
+    var jsonText = await rootBundle.loadString('assets/kabarSeputarSCB.json');
     setState(() {
       data = json.decode(jsonText);
     });
     return 'success';
   }
 
+//=================KODE JOSEP BELOM KELAR =================//
+  Future getBlogPost() async {
+    List<Items> list = [];
+    var fullUrl =
+        '${url}entries?select=fields.title&content_type=blogPost&access_token=${accessToken}';
+    var response = await http.get(fullUrl);
+    var decoded = json.decode(response.body);
+    var items = decoded['items'];
+    // print('items');
+    // print(items);
+    // var test = items.map((json) => print(json)).toList();
+    // print('test');
+    // print(test);
+    // print(items.map((json) => Items.fromJson(json)).toList());
+    // list = items.map<Items>((json) => Items.fromJson(json)).toList();
+    for (Map i in items) {
+      list.add(Items.fromJson(i));
+    }
+    // print("List: ${list}");
+    return list;
+  }
+  //=================KODE JOSEP BELOM KELAR =================//
+
   @override
-  void initState(){
-    super.initState ();
+  void initState() {
+    super.initState();
     this.loadJsonData();
   }
 
@@ -92,10 +119,10 @@ class _KabarSeputarSCBState extends State<KabarSeputarSCB> {
       physics: new NeverScrollableScrollPhysics(),
       itemCount: data.length,
       gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          childAspectRatio: 1.1,
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 1.1,
       ),
       itemBuilder: (BuildContext context, int index) {
         return SingleNews(
@@ -107,6 +134,43 @@ class _KabarSeputarSCBState extends State<KabarSeputarSCB> {
     );
   }
 }
+
+//=================KODE JOSEP BELOM KELAR =================//
+//  Widget listViewWidget(List<Items> article) {
+//      return Container(
+//        child: ListView.builder(
+//            itemCount: 4,
+//            padding: const EdgeInsets.all(2.0),
+//            itemBuilder: (context, position) {
+//              return Card(
+//                  child: ListTile(
+//                title: Text(
+//                  '${article[position].fields}',
+//                  style: TextStyle(
+//                      fontSize: 18.0,
+//                      color: Colors.black,
+//                      fontWeight: FontWeight.bold),
+//                ),
+//                //   leading: Padding(
+//                //     padding: const EdgeInsets.all(8.0),
+//                //     child: SizedBox(
+//                //       child: article[position].urlToImage == null
+//                //           ? Image(
+//                //               image: AssetImage('images/no_image_available.png'),
+//                //             )
+//                //           : Image.network('${article[position].urlToImage}'),
+//                //       height: 100.0,
+//                //       width: 100.0,
+//                //     ),
+//                //   ),
+//                //   onTap: () => _onTapItem(context, article[position]),
+//                // ),
+//              ));
+//            }),
+//      );
+//    }
+
+//=================KODE JOSEP BELOM KELAR =================//
 
 class SingleNews extends StatelessWidget {
   final newsTitle;
@@ -130,41 +194,56 @@ class SingleNews extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-        borderRadius: BorderRadius.circular(5.0),
+      borderRadius: BorderRadius.circular(5.0),
       child: Hero(
-          tag: newsTitle,
-          child: Material(
-            child: InkWell(
-              onTap: _launchURL,
-              child: GridTile(
-                footer: Opacity(
-                  opacity: 0.9,
-                  child: Container(
-                    height: MediaQuery.of(context).size.width /9,
-                    color: Colors.green[50],
-                    child: ListTile(
-                      leading: Padding(
-                        padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
-                        child: Text(
-                          newsTitle,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12),
-                        ),
+        tag: newsTitle,
+        child: Material(
+          child: InkWell(
+            onTap: _launchURL,
+            child: GridTile(
+              footer: Opacity(
+                opacity: 0.9,
+                child: Container(
+                  height: MediaQuery.of(context).size.width / 9,
+                  color: Colors.green[50],
+                  child: ListTile(
+                    leading: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
+                      child: Text(
+                        newsTitle,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12),
                       ),
                     ),
                   ),
                 ),
-                child: Image.network(
-                  newsPicture,
-                  fit: BoxFit.cover,),
-                  ),
-                ),
+              ),
+              child: Image.network(
+                newsPicture,
+                fit: BoxFit.cover,
               ),
             ),
+          )
+//=========================KODE TAMBAHAN JOSEP===========================//
+          //      return Scaffold(
+          //  backgroundColor: Colors.black,
+          //  appBar: AppBar(),
+          //  body: FutureBuilder(
+          //      future: _getBlogPost(),
+          //      builder: (context, snapshot) {
+          //        if (snapshot.hasData) {
+          //          return listViewWidget(snapshot.data);
+          //        }
+          //        return CircularProgressIndicator();
+          //      }),
+//=========================KODE TAMBAHAN JOSEP===========================/
+          ,
+        ),
+      ),
     );
   }
 }
---
+++ lib/models/humasItem.dart
@@ -0,0 +1,25 @@
+class Items {
+  Fields fields;
+
+  Items({this.fields});
+
+  factory Items.fromJson(Map<String, dynamic> json) {
+    print('This items: ');
+    return new Items(
+      fields: Fields.fromJson(json['fields']),
+    );
+    // return Items(Fields.fromJson(json['fields']);
+  }
+}
+
+class Fields {
+  String title;
+  Fields({this.title});
+
+  factory Fields.fromJson(Map<String, dynamic> json) {
+    print(json['title']);
+    return Fields(
+      title: json['title'].toString(),
+    );
+  }
+}
